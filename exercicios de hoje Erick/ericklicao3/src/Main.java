baf0a1f684de103b0e5b0cb7211fe0f86fe815b2
erick <dica.didi8338@gmail.com>
2026-04-21 20:35:50 -0300

    Delete exercicios de hoje Erick directory

--- exercicios de hoje Erick/ericklicao3/src/Main.java
+++
@@ -1,9 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-int a=100;
-double b=a;
-System.out.println("o valor de b é de: "+b);    
-        }
-    }
