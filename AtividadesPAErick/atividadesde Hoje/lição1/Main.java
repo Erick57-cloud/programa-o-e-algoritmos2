256a848c12a7c83cb43faa567b106190e52dfdfc
erick <dica.didi8338@gmail.com>
2026-05-20 12:30:13 -0300

    Add files via upload

---
+++ AtividadesPAErick/atividadesde Hoje/lição1/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class Main{
+    public static void main(String[]args){
+        int i=0;
+        for(i=0; i<=20; i++){
+            if(i % 2 ==0)
+                System.out.println(+i+" é par");
+        }
+    }
+}
