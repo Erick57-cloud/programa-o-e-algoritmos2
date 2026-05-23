d0e78e6d170cb038eb8e7077d170dddf7e8c5a62
erick <dica.didi8338@gmail.com>
2026-05-23 20:43:38 -0300

    Add files via upload

---
+++ AtividadesPAErick/Lições 27-05/facil-2/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class Main {
+    public static void main(String[] args) {
+        int i = 200;
+        while(i>=100){
+            System.out.println(+i);
+            i--;
+        }
+    }
+}
